01800585de98541d5a5d3b7023d0407855957dd2
BRACKE <aurelien.bracke@capgemini.com>
2018-11-26 15:01:38 +0100

    v1.3

--- exo.sh
+++ exo.sh
@@ -1,15 +1,3 @@
 #! /bin/bash
 
-for list in `ls`
-do
-	if [ $1 = $list ] 
-	then
-		mv $1 $1-old1
-		echo "le fichier a ete change"
-	elif [ $1 != $list ]
-	then
-		echo "le fichier nest pas dans le dossier"
-	fi
-done
-
-echo "v1.1"
+echo "test jenkins via git"
